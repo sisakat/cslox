2ed9084fe884823fad7b31baf32986f25b22b385
Stefan Isak <isakhandy@gmail.com>
2021-02-13 19:36:40 +0100

    Arithmetic in VM

--- CLox/chunk.c
+++ CLox/chunk.c
@@ -24,7 +24,7 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, 
             chunk->code, oldCapacity, chunk->capacity);
-        chunk->lines = GROW_ARRAY(uint8_t, 
+        chunk->lines = GROW_ARRAY(int, 
             chunk->lines, oldCapacity, chunk->capacity);
     }
 
